595326df310be483d7e6bea69f5ca8d1b6f3224a
jhan <jhan.perez@correounivalle.edu.co>
2022-11-29 01:07:32 -0500

    validation if exist

--- src/controllers/admin-controller.js
+++ src/controllers/admin-controller.js
@@ -1,55 +1,49 @@
-const {Admin} = require('../models/index')
-var express = require('express');
-const { param } = require('../routes/users');
+const { Admin } = require('../models/index')
 
 module.exports = {
     create_post: async function (req, res) {
         try {
-            const {id_user} = req.params;
+            const { id_user } = req.body
             const admin = await Admin.create({
                 id_user: id_user
-            },{fields:['id_user']})
+            })
             return res.status(201).json({ success: true, admin: JSON.stringify(admin, null, 2) });
         } catch (err) {
             return res.status(500).json({ success: false, error: err, message: "It was not possible to create a new admin" });
-
         }
     },
-    get_all: async function (res) {
+    get_all: async function (req, res) {
         try {
             const admin = await Admin.findAll()
-            return res.status(201).json({ success: true, teacher: JSON.stringify(admin, null, 2) });
+            return res.status(201).json({ success: true, admin: JSON.stringify(admin, null, 2) });
         } catch (err) {
-            if(mistake){
-                return res.status(500).json({ success: false, error: err, message: "Request failed"});
-            }else{
-                return res.status(404).json({ success: false, error: err, message: "It was not possible to get all admins" });
-            }
+            return res.status(500).json({ success: false, error: err, message: "It was not possible to get all admins" });
         }
     },
-    get_one: async function (req,res){
-        try{
-            const {id_admin} = req.params
+    get_one: async function (req, res) {
+        try {
+            const { id_admin } = req.params;
             const admin = await Admin.findByPk(id_admin);
-              return res.status(201).json({ success: true, admin: JSON.stringify(admin, null, 2) });
-        }catch (err){
-            return res.status(500).json({ success: false, error: err, message: "The admin does not exist" });
+            return res.status(201).json({ success: true, admin: JSON.stringify(admin, null, 2) });
+        } catch (err) {
+            return res.status(500).json({ success: false, error: err, message: "It was not possible to get the admin" });
         }
+
     },
     delete_one: async function (req, res) {
         try {
             const { id_admin } = req.params
-            const destroy = await User.destroy({
+            const destroy = await Admin.destroy({
                 where: {
                     id_admin: id_admin
                 }
             })
-            return res.status(201).json({ success: true, user: JSON.stringify(destroy, null, 2) });
+            return res.status(201).json({ success: true, admin: JSON.stringify(destroy, null, 2) });
         } catch (err) {
             return res.status(500).json({ success: false, error: err, message: "It was not possible to delete the admin" });
         }
     },
-        put_one: async function (req, res) {
+    put_one: async function (req, res) {
         try {
             const { id_admin } = req.params;
             const { id_user } = req.body;
@@ -60,7 +54,7 @@ module.exports = {
                     id_admin: id_admin
                 }
             });
-            return res.status(201).json({ success: true, teacher: JSON.stringify(update, null, 2) });
+            return res.status(201).json({ success: true, admin: JSON.stringify(update, null, 2) });
         } catch (error) {
             return res.status(500).json({ success: false, error: err, message: "It was not possible to update the admin" });
         }
--- src/controllers/teacher-controller.js
+++ src/controllers/teacher-controller.js
@@ -1,16 +1,17 @@
 const { Teacher } = require('../models/index')
 
-var mistake;
 
 const exist = (id_teacher) => {
-    const data = Teacher.count({
+    const verify = Teacher.count({
         where: {
             id_teacher: id_teacher
         }
+    }).then(count => {
+        if (count != 0) return true;
+        else return false;
     });
-    
-    if (data.id_teacher != 0) return true;
-    else return false;
+
+    return verify;
 }
 
 module.exports = {
@@ -20,79 +21,91 @@ module.exports = {
             const teacher = await Teacher.create({
                 id_user: id_user
             })
-            return res.status(201).json({ success: true, user: JSON.stringify(teacher, null, 2) });
+            return res.status(201).json({ success: true, teacher: JSON.stringify(teacher, null, 2) });
         } catch (err) {
-            return res.status(500).json({ success: false, error: err });
+            return res.status(500).json({ success: false, error: err, message: "It was not possible to create a new teacher" });
 
         }
     },
-    get_all: async function (res) {
+    get_all: async function (req, res) {
         try {
             const teacher = await Teacher.findAll()
+
+            exist(1).then(exists => {
+                if (exists) {
+                    console.log("ohana existss!");
+                } else {
+                    console.log("buu does not exists!");
+                }
+            });
+
             return res.status(201).json({ success: true, teacher: JSON.stringify(teacher, null, 2) });
+
         } catch (err) {
-            if(mistake){
-                return res.status(500).json({ success: false, error: err, message: "Request failed"});
-            }else{
-                return res.status(404).json({ success: false, error: err, message: "It was not possible to get all admins" });
-            }
+            return res.status(500).json({ success: false, error: err, message: "It was not possible to get all teachers" });
         }
     },
     get_one: async function (req, res) {
         const { id_teacher } = req.params;
-        mistake = exist(id_teacher);
         try {
-            const { id_teacher } = req.params;
             const teacher = await Teacher.findByPk(id_teacher);
             return res.status(201).json({ success: true, teacher: JSON.stringify(teacher, null, 2) });
         } catch (err) {
-            if(mistake){
-                return res.status(500).json({ success: false, error: err, message: "Request failed"});
-            }else{
-                return res.status(404).json({ success: false, error: err, message: "The teacher does not exist" });
-            }
+            exist(id_teacher).then(exists => {
+                if (exists) {
+                    return res.status(500).json({ success: false, error: err, message: "It was not possible to get the teacher" });
+                } else {
+                    return res.status(404).json({ success: false, error: err, message: "Request failed" });
+                }
+            });
+
         }
+
     },
     delete_one: async function (req, res) {
-        const { id_teacher } = req.params;
-        mistake = exist(id_teacher);
+        const { id_teacher } = req.params
         try {
-            const { id_teacher } = req.params
             const destroy = await Teacher.destroy({
                 where: {
                     id_teacher: id_teacher
                 }
             })
-            return res.status(200).json({ success: true, teacher: JSON.stringify(destroy, null, 2) });
+            return res.status(201).json({ success: true, teacher: JSON.stringify(destroy, null, 2) });
         } catch (err) {
-            if(mistake){
-                return res.status(500).json({ success: false, error: err, message: "Request failed"});
-            }else{
-                return res.status(404).json({ success: false, error: err, message: "There is not a teacher with this id" });
-            }
+            exist(id_teacher).then(exists => {
+                if (exists) {
+                    return res.status(500).json({ success: false, error: err, message: "It was not possible to delete the teacher" });
+                } else {
+                    return res.status(404).json({ success: false, error: err, message: "Request failed" });
+                }
+            });
         }
     },
     put_one: async function (req, res) {
         const { id_teacher } = req.params;
-        mistake = exist(id_teacher);
         try {
-            const { id_teacher } = req.params;
-            const { new_id_user } = req.body;
-        
-            const update = await User.update({
-                id_user: new_id_user
-            },{
-                where:{
-                    id_teacher:id_teacher
+            const { id_user } = req.body;
+            const update = await Teacher.update({
+                id_user: id_user
+            }, {
+                where: {
+                    id_teacher: id_teacher
                 }
             });
             return res.status(201).json({ success: true, teacher: JSON.stringify(update, null, 2) });
         } catch (error) {
-            if(mistake){
-                return res.status(500).json({ success: false, error: err, message: "Request failed"}); 
-            }else{
-                return res.status(404).json({ success: false, error: err, message: "The teacher does not exist" });
-            }
+            exist(id_teacher).then(exists => {
+                if (exists) {
+                    return res.status(500).json({ success: false, error: err, message: "It was not possible to update the teacher" });
+                } else {
+                    return res.status(404).json({ success: false, error: err, message: "Request failed" });
+                }
+            });
         }
     }
 }
+
+
+
+
+
