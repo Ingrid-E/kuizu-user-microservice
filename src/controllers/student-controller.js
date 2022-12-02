b0ace72fbe17baf6f1b742195bd0a2aa6569d2fa
jhan <jhan.perez@correounivalle.edu.co>
2022-12-02 15:22:40 -0500

    messages changed

--- src/controllers/student-controller.js
+++ src/controllers/student-controller.js
@@ -7,57 +7,69 @@ module.exports = {
             const student = await Student.create({
                 id_user: id_user
             })
-            return res.status(201).json({ success: true, student: JSON.stringify(student, null, 2) });
+            return res.status(201).json({ success: true, data: {title: "Student created!", id_user: user.id_user}});
         } catch (err) {
-            return res.status(500).json({ success: false, error: err, message: "It was not possible to create a new student" });
+            return res.status(500).json({ success: false, data: {title: "Internal Server error", error: err.message}});
 
         }
     },
     get_all: async function (req,res){
         try{
             const student = await Student.findAll()
-            return res.status(201).json({ success: true, student: JSON.stringify(student, null, 2) });
+            if(student.length === 0){
+                return res.status(404).json({success: false, data: {title: "Students not found"}})
+            }
+            return res.status(200).json({ success: true, data: {count: users.length, users}});
         }catch (err){
-            return res.status(500).json({ success: false, error: err, message: "It was not possible to get all students" });
+            return res.status(500).json({ success: false, data: {title: "Internal Server error", error: err.message}});
         }
     },
     get_one: async function (req,res){
         try{
             const {id_student} = req.params;
             const student = await Student.findByPk(id_student);
-              return res.status(201).json({ success: true, student: JSON.stringify(student, null, 2) });
+            if(student === null){
+                return res.status(404).json({success: false, data: {title: "Student not found"}})
+            }
+            return res.status(200).json({ success: true, data: {user}});
         }catch (err){
-            return res.status(500).json({ success: false, error: err, message: "It was not possible to get the student" });
+            return res.status(500).json({ success: false, data: {title: "Internal Server error", error: err.message}});
         }
 
     },
     delete_one: async function (req, res) {
         try {
             const { id_student } = req.params
-            const destroy = await Student.destroy({
+            const student = await Student.destroy({
                 where: {
                     id_student: id_student
                 }
             })
-            return res.status(201).json({ success: true, student: JSON.stringify(destroy, null, 2) });
+            if(student === 0){
+                return res.status(404).json({success: false, data: {title: "Student not found"}})
+            }
+            return res.status(200).json({ success: true, data: {title: "Student deleted"}});
         } catch (err) {
-            return res.status(500).json({ success: false, error: err, message: "It was not possible to delete the student" });
+            return res.status(500).json({ success: false, data: {title: "Internal Server error", error: err.message}});
         }
     },
     put_one: async function (req, res) {
         try {
             const { id_student } = req.params;
             const { id_user } = req.body;
-            const update = await Admin.update({
+            const student = await Student.update({
                 id_user: id_user
             }, {
                 where: {
                     id_student: id_student
                 }
             });
-            return res.status(201).json({ success: true, teacher: JSON.stringify(update, null, 2) });
-        } catch (error) {
-            return res.status(500).json({ success: false, error: err, message: "It was not possible to update the student" });
+            if(student[0] === 0){
+                return res.status(404).json({success: false, data: {title: "Student not found"}})
+            }
+            return res.status(200).json({ success: true, data: {title: "Student updated"}});
+        } catch (err) {
+            return res.status(500).json({ success: false, data: {title: "Internal Server error", error: err.message}});
         }
     }
 }
